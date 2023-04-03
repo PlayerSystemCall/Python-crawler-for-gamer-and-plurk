6287a90e6f56fcbd160e237db265a41a5c468de8
Player_SystemCall <playersystemcall@gmail.com>
2023-04-03 22:08:40 +0800

    第五次test env

--- envtest.py
+++ envtest.py
@@ -2,12 +2,5 @@
 import os
 from dotenv import load_dotenv
 
-A = load_dotenv()
-print("A", A)
-
 GCP_PROJECT_ID = os.getenv('GCP_PROJECT_ID')
-print("GCP_PROJECT_ID", GCP_PROJECT_ID)
-SERVICE_ACCOUNT_FILE = os.getenv('SERVICE_ACCOUNT_FILE')
-print("SERVICE_ACCOUNT_FILE", SERVICE_ACCOUNT_FILE)
-STORAGE_BUCKET_NAME = os.getenv('STORAGE_BUCKET_NAME')
-print("STORAGE_BUCKET_NAME", STORAGE_BUCKET_NAME)
+print("GCP_PROJECT_ID=", GCP_PROJECT_ID)
