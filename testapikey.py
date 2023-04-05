d7552be20506afb6fd663decf1bd43be1f7cc528
Player_SystemCall <playersystemcall@gmail.com>
2023-04-06 00:08:35 +0800

    第十九之五次test env

--- testapikey.py
+++ testapikey.py
@@ -2,10 +2,10 @@
 import json
 
 file = open("cypress.json")
-A = json.load(file.read())
+A = file.read()
 print(A)
 
-if A == {"a": 1, "b": 2, "c": 3}:
+if A == '{"a": 1, "b": 2, "c": 3}':
     print("YES")
 else:
     print("NO")
