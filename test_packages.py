a6248d2f9df510ba25256a115898862e9e84e268
Player_SystemCall <playersystemcall@gmail.com>
2023-04-24 17:50:06 +0800

    第十一次更新get_plurk_data.yml和第十五次更新get_gamer_data.yml

---
+++ test_packages.py
@@ -0,0 +1,97 @@
+# test package
+
+try:
+    import os
+    print("success to import os")
+except:
+    print("fail to import os")
+
+try:
+    import re
+    print("success to import re")
+except:
+    print("fail to import re")
+
+try:
+    import json
+    print("success to import json")
+except:
+    print("fail to import json")
+
+try:
+    import socket
+    print("success to import socket")
+except:
+    print("fail to import socket")
+
+try:
+    import requests
+    print("success to import requests")
+except:
+    print("fail to import requests")
+
+try:
+    import pygsheets
+    print("success to import pygsheets")
+except:
+    print("fail to import pygsheets")
+
+try:
+    import Python_crawler_sub_ver1
+    print("success to import Python_crawler_sub_ver1")
+except:
+    print("fail to import Python_crawler_sub_ver1")
+
+try:
+    from lxml import etree
+    print("success to from lxml import etree")
+except:
+    print("fail to from lxml import etree")
+
+try:
+    from dotenv import load_dotenv
+    print("success to from dotenv import load_dotenv")
+except:
+    print("fail to from dotenv import load_dotenv")
+
+try:
+    from datetime import datetime
+    print("success to from datetime import datetime")
+except:
+    print("fail to from datetime import datetime")
+
+try:
+    from datetime import timedelta
+    print("success to from datetime import datetime, timedelta")
+except:
+    print("fail to from datetime import datetime, timedelta")
+
+try:
+    import pytz
+    print("success to import pytz")
+except:
+    print("fail to import pytz")
+
+try:
+    import time
+    print("success to import time")
+except:
+    print("fail to import time")
+
+try:
+    import ntplib
+    print("success to import ntplib")
+except:
+    print("fail to import ntplib")
+
+try:
+    import psutil
+    print("success to import psutil")
+except:
+    print("fail to import psutil")
+
+try:
+    import ipaddress
+    print("success to import ipaddress")
+except:
+    print("fail to import ipaddress")
